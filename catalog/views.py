60c92cff78a3d1900f859735570c4dac08250fa9
Archita Totla <architatotla@gmail.com>
2020-08-19 18:46:14 +0530

    signup functionality added

--- catalog/views.py
+++ catalog/views.py
@@ -20,23 +20,23 @@ from django.contrib.auth import login,authenticate
 #@login_required
 def index(request):
     """View function for home page of our site."""
-    
+
     #Generate counts of some of the main objects
     num_books = Book.objects.all().count()
     num_instances = BookInstance.objects.all().count()
     num_genres = Genre.objects.count()
-    
+
     #Available books (status = 'a')
     num_instances_available = BookInstance.objects.filter(status__exact = 'a').count()
     num_instances_on_loan = BookInstance.objects.filter(status__exact = 'o').count()
-    
+
     #The 'all' is implied by default.
     num_authors = Author.objects.count()
-    
+
     #Number of visits to this view, as counted in the session variable.
     num_visits = request.session.get('num_visits', 0)
     request.session['num_visits'] = num_visits + 1
-    
+
     context = {
         'num_books': num_books,
         'num_instances_on_loan': num_instances_on_loan,
@@ -45,73 +45,73 @@ def index(request):
         'num_genres': num_genres,
         'num_visits': num_visits,
     }
-    
+
     #Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
 
 class BookListView(generic.ListView):
     model = Book
     paginate_by = 12
-    
+
 class BookDetailView(generic.DetailView):
     model = Book
-    
+
 class AuthorListView(generic.ListView):
     model = Author
     paginate_by = 10
-    
+
 class AuthorDetailView(generic.DetailView):
     model = Author
-    
+
 class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
     """Generic class-based view listing books on loan to current user."""
     model = BookInstance
     template_name = 'catalog/bookinstance_list_borrowed_user.html'
     paginate_by=10
-    
+
     def get_queryset(self):
         return BookInstance.objects.filter(borrower = self.request.user).filter(status__exact='o').order_by('due_back')
-    
+
 class allLoanedBooks(PermissionRequiredMixin, generic.ListView):
     """Generic class-based view listing all the books loaned to all the users."""
     model=BookInstance
     template_name = 'catalog/books_list_allborrowed.html'
     paginate_by=10
     permission_required = 'catalog.can_mark_returned'
-    
+
     def get_queryset(self):
         return BookInstance.objects.filter(status__exact='o').order_by('borrower','due_back')
-    
+
 @permission_required('catalog.can_mark_returned')
 def renew_book_librarian(request, pk):
     """View function for renewing a specific BookInstance by librarian."""
     book_instance = get_object_or_404(BookInstance, pk=pk)
-    
+
     #If this is a POST request then process the Form data
     if request.method == 'POST':
-        
+
         #Create a form instance and populate it with data from the request (binding):
         form = RenewBookForm(request.POST)
-        
+
         #Check if the form is valid:
         if form.is_valid():
             #process the data in form.cleaned_data as required (here we just write it to the model due_back field)
             book_instance.due_back = form.cleaned_data['renewal_date']
             book_instance.save()
-            
+
             #redirect to a new URL
             return HttpResponseRedirect(reverse('all-borrowed'))
-        
+
     #If this is a GET (or any other method) create the default form.
     else:
         proposed_renewal_date = datetime.date.today() + datetime.timedelta(weeks=3)
         form = RenewBookForm(initial={'renewal_date': proposed_renewal_date})
-        
+
     context = {
         'form': form,
         'book_instance': book_instance,
     }
-    
+
     return render(request, 'catalog/book_renew_librarian.html', context)
 
 class AuthorCreate(CreateView):
@@ -126,7 +126,7 @@ class AuthorUpdate(UpdateView):
 class AuthorDelete(DeleteView):
     model = Author
     success_url = reverse_lazy('authors')
-    
+
 class BookCreate(CreateView):
     model = Book
     fields = '__all__'
@@ -139,16 +139,20 @@ class BookUpdate(UpdateView):
 class BookDelete(DeleteView):
     model = Book
     success_url = reverse_lazy('books')
-    
+
 def signup_view(request):
     form = SignUpForm(request.POST)
+    print("post request")
     if form.is_valid():
-        form.save()
+        print("valid form")
+        user = form.save()
+        user.refresh_from_db()
+        user.profile.first_name = form.cleaned_data.get('first_name')
+        user.profile.last_name = form.cleaned_data.get('last_name')
+        user.save()
         username = form.cleaned_data.get('username')
         password = form.cleaned_data.get('password1')
         user = authenticate(username = username, password = password)
-        if user is not None:
-            User.objects.create_user(username = username, password = password)
         login(request,user)
         return redirect('index')
     else:
